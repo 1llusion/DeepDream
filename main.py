20c95f9578c12ed183d6f70fc5b53d4f527f7029
Chris <chris.darlington.music@gmail.com>
2020-03-23 20:37:09 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,120 @@
+from __future__ import print_function
+import os
+from io import BytesIO
+import numpy as np
+from functools import partial
+import PIL.Image
+from IPython.display import clear_output, Image, display, HTML
+
+import tensorflow.compat.v1 as tf
+
+model_fn = '/spell/tensorflow_inception_graph.pb'
+
+# creating TensorFlow session and loading the model from the model_fn file 
+graph = tf.Graph()
+sess = tf.InteractiveSession(graph=graph)
+with tf.gfile.FastGFile(model_fn, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+t_input = tf.placeholder(np.float32, name='input') # define the input tensor
+imagenet_mean = 117.0
+t_preprocessed = tf.expand_dims(t_input-imagenet_mean, 0)
+tf.import_graph_def(graph_def, {'input':t_preprocessed})
+
+layers = [op.name for op in graph.get_operations() if op.type=='Conv2D' and 'import/' in op.name]
+feature_nums = [int(graph.get_tensor_by_name(name+':0').get_shape()[-1]) for name in layers]
+
+print('Number of layers', len(layers))
+print('Total number of feature channels:', sum(feature_nums))
+
+def T(layer):
+    '''Helper for getting layer output tensor'''
+    return graph.get_tensor_by_name("import/%s:0"%layer)
+    
+layer=layers[4]
+print(layer)
+layer = layer.split("/")[1]
+print(layer)
+
+for l, layer in enumerate(layers):
+    layer = layer.split("/")[1]
+    num_channels = T(layer).shape[3]
+    print(layer, num_channels)
+    
+img_noise = np.random.uniform(size=(224,224,3)) + 100.0
+
+def showarray(a, fmt='jpeg', img_num=0):
+    '''create a jpeg file from an array a and visualize it'''
+    # clip the values to be between 0 and 255
+    a = np.uint8(np.clip(a, 0, 1)*255)
+    f = BytesIO()
+    PIL.Image.fromarray(a).save(f, fmt)
+    PIL.Image.fromarray(a).save("output/" + str(img_num) + ".jpg", fmt)
+    display(Image(data=f.getvalue()))
+    
+def render_deepdream(t_obj, img0=img_noise,
+                     iter_n=10, step=1.1, octave_n=4, octave_scale=1.1):
+    t_score = tf.reduce_mean(t_obj) # defining the optimization objective
+    t_grad = tf.gradients(t_score, t_input)[0] # behold the power of automatic differentiation!
+
+    # split the image into a number of octaves getting smaller and smaller images
+    img = img0
+    octaves = []
+    for i in range(octave_n-1):
+        hw = img.shape[:2] #image height and width
+        lo = resize(img, np.int32(np.float32(hw)/octave_scale)) #low frequency parts (smaller image)
+        hi = img-resize(lo, hw) #high frequency parts (details)
+        img = lo # next iteration rescale this one
+        octaves.append(hi) # add the details to octaves
+    
+    # generate details octave by octave from samll image to large
+    for octave in range(octave_n):
+        if octave>0:
+            hi = octaves[-octave]
+            img = resize(img, hi.shape[:2])+hi
+        for i in range(iter_n):
+            g = calc_grad_tiled(img, t_grad)
+            img += g*(step / (np.abs(g).mean()+1e-7))
+            print('.',end = ' ')
+            #clear_output()
+            showarray(img/255.0, img_num=(octave + 1) * (i + 1))
+
+def tffunc(*argtypes):
+    '''Helper that transforms TF-graph generating function into a regular one.
+    See "resize" function below.
+    '''
+    placeholders = list(map(tf.placeholder, argtypes))
+    def wrap(f):
+        out = f(*placeholders)
+        def wrapper(*args, **kw):
+            return out.eval(dict(zip(placeholders, args)), session=kw.get('session'))
+        return wrapper
+    return wrap
+
+# Helper function that uses TF to resize an image
+def resize(img, size):
+    img = tf.expand_dims(img, 0)
+    return tf.image.resize_bilinear(img, size)[0,:,:,:]
+resize = tffunc(np.float32, np.int32)(resize)
+
+def calc_grad_tiled(img, t_grad, tile_size=512):
+    '''Compute the value of tensor t_grad over the image in a tiled way.
+    Random shifts are applied to the image to blur tile boundaries over 
+    multiple iterations.'''
+    sz = tile_size
+    h, w = img.shape[:2] # size of the image
+    sx, sy = np.random.randint(sz, size=2) # random shift numbers generated
+    img_shift = np.roll(np.roll(img, sx, 1), sy, 0) #shift the whole image. np.roll = Roll array elements along a given axis
+    grad = np.zeros_like(img)
+    for y in range(0, max(h-sz//2, sz),sz):
+        for x in range(0, max(w-sz//2, sz),sz):
+            sub = img_shift[y:y+sz,x:x+sz] # get the image patch (tile)
+            g = sess.run(t_grad, {t_input:sub}) # calculate the gradient only in the image patch not in the whole image!
+            grad[y:y+sz,x:x+sz] = g # put the whole gradient together from the tiled gradients g
+    return np.roll(np.roll(grad, -sx, 1), -sy, 0) # shift back
+
+img0 = PIL.Image.open('/spell/didge.jpg')
+img0 = np.float32(img0)
+showarray(img0/255.0)
+
+render_deepdream(tf.square(T('mixed4c')), img0, iter_n=10)
